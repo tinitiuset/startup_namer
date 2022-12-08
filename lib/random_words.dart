fa90f1607c7d9a50bc6cd1aba4118f88a1873a80
tinitiuset <tinitiuset@gmail.com>
2022-12-08 12:28:09 +0100

    Created new screen

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -9,46 +9,73 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-
   final _suggestions = <WordPair>[];
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, i)
-      {
-        if (i.isOdd) return const Divider();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup name generator'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.list),
+              onPressed: _pushSaved,
+              tooltip: 'Saved Suggestions',
+            )
+          ],
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
 
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        final alredySaved = _saved.contains(_suggestions[index]);
-        return ListTile(
-          title: Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
-          ),
-          trailing: Icon(
-            alredySaved ? Icons.favorite : Icons.favorite_border,
-            color: alredySaved ? Colors.red : null,
-            semanticLabel: alredySaved ? 'Remove from saved' : 'Save',
-          ),
-          onTap: () {
-            setState(() {
-              if (alredySaved) {
-                _saved.remove(_suggestions[index]);
-              } else {
-                _saved.add(_suggestions[index]);
-              }
-            });
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            final alredySaved = _saved.contains(_suggestions[index]);
+            return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                alredySaved ? Icons.favorite : Icons.favorite_border,
+                color: alredySaved ? Colors.red : null,
+                semanticLabel: alredySaved ? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
+                setState(() {
+                  if (alredySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                });
+              },
+            );
           },
-        );
+        ));
+  }
 
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) {
+        final tiles = _saved.map((pair) {
+          return ListTile(
+              title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ));
+        });
+        final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+            : <Widget>[];
+        return Scaffold(
+            appBar: AppBar(title: const Text('Saved suggestions')),
+            body: ListView(children: divided));
       },
-
-    );
+    ));
   }
 }
